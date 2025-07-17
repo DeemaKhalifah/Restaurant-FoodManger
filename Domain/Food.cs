f11290b1ccfadb1e2c26ac42aeea23265151e846
Deema Khalifah <deema.khalifah@gmail.com>
2025-07-17 13:12:55 +0200

    Update Food.cs

--- Domain/Food.cs
+++ Domain/Food.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,29 +8,23 @@ namespace Domain
 {
     public class Food
     {
-        public static int counter = 1;
-
+        public static int Counter = 1;
+        public int Id { get; private set; }
         public string Name { get; set; }
-        public int Id { get; set; }
-        public bool isDeleted { get; set; }
-
+        public bool IsDeleted { get; set; }
         public double Price { get; set; }
-
         public string Category { get; set; }
-        public bool isUpdated { get; set; }
 
 
-        public Food(string name, bool isDeleted, double price, string category, bool isUpdated)
+        public Food(string name, double price, string category)
         {
-
-            this.Name = name;
-            this.Id = counter++;
-            this.isDeleted = isDeleted;
-            this.Price = price;
-            this.Category = category;
-            this.isUpdated = isUpdated;
+            Id = Counter++;
+            Name = name;
+            Price = price;
+            Category = category;
+            IsDeleted = false;
         }
-
-
     }
+
+
 }
